98b8e4dad15372bb98bb4f8aa803f88569e7df52
dafinaliza <ababaniyi@rnsolutions.com>
2012-08-13 17:03:34 -0400

    Updating Directory Structure

---
+++ src/main/java/net/mscchoir/donate/util/Status.java
@@ -0,0 +1,15 @@
+// File:    Status.java
+// Author:  Ayodeji Babaniyi
+// Date:    03/05/2011
+// Purpose: Utility class used for storing exceptions and displaying them to the users.
+package net.mscchoir.donate.util;
+import java.util.ArrayList;
+import java.util.Iterator;
+public class Status {
+   //The list of Exception objects
+   private ArrayList exceptions;
+   public Status() {exceptions=new ArrayList();}
+   public boolean isSuccessful() {return (exceptions.isEmpty());}
+   public void addException(Exception ex) {exceptions.add(ex);}
+   public Iterator getExceptions() {return exceptions.iterator();}
+}
---
+++ src/main/java/net/mscchoir/donate/util/ValidInput.java
@@ -0,0 +1,62 @@
+// File:    ValidInput.java
+// Author:  Ayodeji Babaniyi
+// Date:    05/01/2011
+// Purpose: Utility class used for verifying different types of user input is valid
+
+package net.mscchoir.donate.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidInput
+{
+     public static boolean isValidEmail(String emailAddress)
+     {
+           String  expression="^[\\w\\-]([\\.\\w])+[\\w]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+           CharSequence input = emailAddress;
+           Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);
+           Matcher matcher = pattern.matcher(input);
+           return matcher.matches();
+     }
+
+     public static boolean isValidZipCode(String emailAddress)
+     {
+           String  expression="\\d{5}(-\\d{4})?";
+           CharSequence input = emailAddress;
+           Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);
+           Matcher matcher = pattern.matcher(input);
+           return matcher.matches();
+     }
+
+     public static boolean isValidMoney(String s)
+     {
+        try
+        {
+            double d = Double.parseDouble(s);
+            
+            return (d > 0);
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+     }
+
+     public static boolean isValidInteger(String s)
+     {
+        try
+        {
+            int i = Integer.parseInt(s);
+            return (i > 0);
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+     }
+
+    public static boolean isValidString(String s)
+    {
+        return ((s != null) && !s.isEmpty());
+    }
+}
